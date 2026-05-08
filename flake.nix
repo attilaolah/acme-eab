dfeccb54eb43b9674f1487817b871d5e05b08d0b
Attila Oláh <attila@dorn.haus>
2026-05-08 08:18:20 +0200

    Add acme-eab-add CLI tool for ACME External Account Binding (#4)
    * feat: add acme-eab-add
    
    * refactor: reword README, add coderabbit badge
    
    * feat: add basic actions
    
    * fix: handle db close and reference keys
    
    * fix: bump pgx dependency
    
    * fix: derive nix package version
    
    * fix: delete empty provisioner index
    
    * chore: ignore local build outputs
    
    * docs: update readme
    
    * docs: add terse function comments
    
    * ci: add workflow job timeouts

---
+++ flake.nix
@@ -0,0 +1,56 @@
+{
+  description = "Smallstep ACME EAB helper";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+  outputs = {
+    self,
+    nixpkgs,
+    ...
+  }: let
+    inherit (nixpkgs) lib;
+
+    systems = [
+      "aarch64-darwin"
+      "aarch64-linux"
+      "x86_64-darwin"
+      "x86_64-linux"
+    ];
+
+    forAllSystems = lib.genAttrs systems;
+  in {
+    packages = forAllSystems (system: let
+      pkgs = import nixpkgs {inherit system;};
+    in rec {
+      acme-eab-add = pkgs.buildGoModule {
+        pname = "acme-eab-add";
+        version = self.shortRev or self.dirtyShortRev or "dirty";
+
+        src = ./.;
+        vendorHash = "sha256-gMwKX6rwKia4+hhbMJrJLhXidT47JGsd0//fyBemRVA=";
+
+        subPackages = ["cmd/acme-eab-add"];
+
+        meta = {
+          description = "Write Smallstep ACME External Account Binding credentials into the Step CA database";
+          homepage = "https://github.com/attilaolah/acme-eab";
+          license = lib.licenses.mit;
+          mainProgram = "acme-eab-add";
+        };
+      };
+
+      default = acme-eab-add;
+    });
+
+    devShells = forAllSystems (system: let
+      pkgs = import nixpkgs {inherit system;};
+    in {
+      default = pkgs.mkShell {
+        packages = with pkgs; [
+          go
+          gopls
+        ];
+      };
+    });
+  };
+}
---
+++ go.mod
@@ -0,0 +1,33 @@
+module github.com/attilaolah/acme-eab
+
+go 1.25.0
+
+require (
+	github.com/pkg/errors v0.9.1
+	github.com/smallstep/nosql v0.8.0
+)
+
+require (
+	filippo.io/edwards25519 v1.1.1 // indirect
+	github.com/AndreasBriese/bbloom v0.0.0-20190825152654-46b345b51c96 // indirect
+	github.com/cespare/xxhash v1.1.0 // indirect
+	github.com/dgraph-io/badger v1.6.2 // indirect
+	github.com/dgraph-io/badger/v2 v2.2007.4 // indirect
+	github.com/dgraph-io/ristretto v0.0.3-0.20200630154024-f66de99634de // indirect
+	github.com/dgryski/go-farm v0.0.0-20200201041132-a6ae2369ad13 // indirect
+	github.com/dustin/go-humanize v1.0.0 // indirect
+	github.com/go-sql-driver/mysql v1.9.3 // indirect
+	github.com/golang/protobuf v1.5.0 // indirect
+	github.com/golang/snappy v0.0.3 // indirect
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
+	github.com/jackc/pgx/v5 v5.9.2 // indirect
+	github.com/jackc/puddle/v2 v2.2.2 // indirect
+	github.com/klauspost/compress v1.12.3 // indirect
+	go.etcd.io/bbolt v1.4.3 // indirect
+	golang.org/x/net v0.47.0 // indirect
+	golang.org/x/sync v0.18.0 // indirect
+	golang.org/x/sys v0.38.0 // indirect
+	golang.org/x/text v0.31.0 // indirect
+	google.golang.org/protobuf v1.33.0 // indirect
+)
